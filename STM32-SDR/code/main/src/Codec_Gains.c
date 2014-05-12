46b648e3f12d04f4737613de0a5d736c3dddfbc5
Jack (W0FNQ) <9artus@gmail.com>
2014-05-11 19:51:58 -0500

    Typo on HPA

--- STM32-SDR/code/main/src/Codec_Gains.c
+++ STM32-SDR/code/main/src/Codec_Gains.c
@@ -220,7 +220,7 @@ void Connect_Sidetone_Input(void)  // Power Up MAR and HP
 	I2C_WriteRegister(CODEC_ADDRESS, 0x00, 0x01);	//Page Select
 	Delay(Codec_Pause);
 
-	I2C_WriteRegister(CODEC_ADDRESS, 0x08, 0x01);  //MAR to HPL and LDAC AFIR not routed to HPL
+	I2C_WriteRegister(CODEC_ADDRESS, 0x0c, 0x01);  //MAR to HPL and LDAC AFIR not routed to HPL
 	Delay(Codec_Pause);
 
 	I2C_WriteRegister(CODEC_ADDRESS, 0x0d, 0x02);  //MAR to HPR and RDAC AFIR not routed to HPR
