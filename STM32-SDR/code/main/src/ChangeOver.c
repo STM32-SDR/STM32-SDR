79bb3920ac682d251ccf51d05130d08305678a14
Jack (W0FNQ) <9artus@gmail.com>
2014-10-26 11:42:06 -0500

    SSB Changeover feedback bug

--- STM32-SDR/code/main/src/ChangeOver.c
+++ STM32-SDR/code/main/src/ChangeOver.c
@@ -197,8 +197,9 @@ void Xmit_SSB_Sequence(void)
 	Mute_LO();
 	s_inTxMode = 1;
 	AGC_On = 0;  //Turn off AGC so that DAC is held constant during transmit
-	//Init_AGC();  //added JDG
-	Disconnect_PGA();
+	Init_AGC();  //added JDG
+	Disconnect_Sidetone_Input();  //added JDG
+	//Disconnect_PGA();
 	Set_DAC_DVC(15); //SSB Xmit DAC Gain
 	Set_ADC_DVC(0);
 	Connect_Microphone_Input();
@@ -230,8 +231,8 @@ void Xmit_PSK_Sequence(void)
 	Mute_LO();
 	s_inTxMode = 1;
 	AGC_On = 0;  //Turn off AGC so that DAC is held constant during transmit
-	//Init_AGC();  //added JDG
-	Disconnect_PGA();
+	Disconnect_Sidetone_Input();  //added JDG
+	//Disconnect_PGA();
 	Set_DAC_DVC(12); //PSK Xmit DAC Gain
 	GPIO_WriteBit(GPIOD, GPIO_Pin_3, Bit_RESET);  //Make PTT_Out Low,Remember FET Inversion
 	Delay(1000);
